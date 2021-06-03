00555ce7be1593d82c60b4d353750cc91375e961
Gfilinic <40752655+Gfilinic@users.noreply.github.com>
2021-06-03 11:56:55 +0200

    Dodan UI za kafić zajedno sa stolovima. Također dodana opcija za dodavanje i brisanje stolova.

---
+++ Software/PI_2021_Kafic/UCStol.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PI_2021_Kafic
+{
+    public partial class UCStol : UserControl
+    {
+        private Narudzbe narudzbe;
+        public UCStol()
+        {
+            InitializeComponent();
+            narudzbe = null;
+        }
+        
+        public string ImeStola
+        {
+            get { return lblImeStola.Text; }
+            set { lblImeStola.Text = value; }
+        }
+
+        private void pbStol_Click(object sender, EventArgs e)
+        {
+            if (narudzbe == null)
+            {
+                narudzbe = new Narudzbe(ImeStola);
+                narudzbe.ShowDialog();
+            }
+            else narudzbe.ShowDialog();
+        }
+    }
+}
