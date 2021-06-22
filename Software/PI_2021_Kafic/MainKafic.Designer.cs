d8853444ce8cd693cbcf901d2d33b26cb682908e
Lea Masnec <leamasnec@gmail.com>
2021-06-22 17:06:27 +0200

    no message

---
+++ Software/PI_2021_Kafic/MainKafic.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace PI_2021_Kafic
+{
+    partial class MainKafic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.racuniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.upravaljnjeStolovimaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.skladišteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stanjeNaSkladištuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.upravljanjeNormativimaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.upravljanjeNamirnicamaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.upravljanjeArtiklimaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.racuniToolStripMenuItem,
+            this.upravaljnjeStolovimaToolStripMenuItem,
+            this.skladišteToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // racuniToolStripMenuItem
+            // 
+            this.racuniToolStripMenuItem.Name = "racuniToolStripMenuItem";
+            this.racuniToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.racuniToolStripMenuItem.Text = "Racuni";
+            // 
+            // upravaljnjeStolovimaToolStripMenuItem
+            // 
+            this.upravaljnjeStolovimaToolStripMenuItem.Name = "upravaljnjeStolovimaToolStripMenuItem";
+            this.upravaljnjeStolovimaToolStripMenuItem.Size = new System.Drawing.Size(133, 20);
+            this.upravaljnjeStolovimaToolStripMenuItem.Text = "Upravaljnje stolovima";
+            this.upravaljnjeStolovimaToolStripMenuItem.Click += new System.EventHandler(this.upravaljnjeStolovimaToolStripMenuItem_Click);
+            // 
+            // skladišteToolStripMenuItem
+            // 
+            this.skladišteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stanjeNaSkladištuToolStripMenuItem,
+            this.upravljanjeNormativimaToolStripMenuItem,
+            this.upravljanjeNamirnicamaToolStripMenuItem,
+            this.upravljanjeArtiklimaToolStripMenuItem});
+            this.skladišteToolStripMenuItem.Name = "skladišteToolStripMenuItem";
+            this.skladišteToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.skladišteToolStripMenuItem.Text = "Skladište";
+            // 
+            // stanjeNaSkladištuToolStripMenuItem
+            // 
+            this.stanjeNaSkladištuToolStripMenuItem.Name = "stanjeNaSkladištuToolStripMenuItem";
+            this.stanjeNaSkladištuToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.stanjeNaSkladištuToolStripMenuItem.Text = "Stanje na skladištu";
+            // 
+            // upravljanjeNormativimaToolStripMenuItem
+            // 
+            this.upravljanjeNormativimaToolStripMenuItem.Name = "upravljanjeNormativimaToolStripMenuItem";
+            this.upravljanjeNormativimaToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.upravljanjeNormativimaToolStripMenuItem.Text = "Upravljanje normativima";
+            this.upravljanjeNormativimaToolStripMenuItem.Click += new System.EventHandler(this.upravljanjeNormativimaToolStripMenuItem_Click);
+            // 
+            // upravljanjeNamirnicamaToolStripMenuItem
+            // 
+            this.upravljanjeNamirnicamaToolStripMenuItem.Name = "upravljanjeNamirnicamaToolStripMenuItem";
+            this.upravljanjeNamirnicamaToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.upravljanjeNamirnicamaToolStripMenuItem.Text = "Upravljanje namirnicama";
+            this.upravljanjeNamirnicamaToolStripMenuItem.Click += new System.EventHandler(this.upravljanjeNamirnicamaToolStripMenuItem_Click);
+            // 
+            // upravljanjeArtiklimaToolStripMenuItem
+            // 
+            this.upravljanjeArtiklimaToolStripMenuItem.Name = "upravljanjeArtiklimaToolStripMenuItem";
+            this.upravljanjeArtiklimaToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.upravljanjeArtiklimaToolStripMenuItem.Text = "Upravljanje artiklima";
+            // 
+            // MainKafic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 541);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainKafic";
+            this.Text = "MainKafic";
+            this.Activated += new System.EventHandler(this.MainKafic_Activated);
+            this.Load += new System.EventHandler(this.MainKafic_Load);
+            this.ResizeEnd += new System.EventHandler(this.MainKafic_ResizeEnd);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem racuniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem upravaljnjeStolovimaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem skladišteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stanjeNaSkladištuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem upravljanjeNormativimaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem upravljanjeNamirnicamaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem upravljanjeArtiklimaToolStripMenuItem;
+    }
+}
---
+++ Software/PI_2021_Kafic/MainKafic.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PI_2021_Kafic
+{
+    public partial class MainKafic : Form
+    {
+        List<UCStol> listaStolova;
+        Kafic kafic;
+        public MainKafic()
+        {
+            InitializeComponent();
+            listaStolova = new List<UCStol>();
+            kafic = DohvatiKafic(1);
+            
+            RefreshStolovi();
+
+        }
+
+        private Kafic DohvatiKafic(int id)
+        {
+            using (var context = new Entities())
+            {
+                var query = from k in context.Kafic
+                            where k.ID_Kafic == id
+                            select k;
+                return query.Single();
+            }
+        }
+
+        private void RefreshStolovi()
+        {
+            IzbrisiStolove();
+            int point1 = 0;
+            int point2 = 27;
+            List<Stol> lista = new List<Stol>();
+            using (var context = new Entities())
+            {
+                
+                var query = from s in context.Stol
+                            where s.Kafic_ID == kafic.ID_Kafic
+                            select s;
+                lista = query.ToList();
+                foreach(Stol stol in lista)
+               
+                {
+                    UCStol uCStol = new UCStol();
+                    if (point1 + 193 < this.Size.Width)
+                    {
+                        uCStol.Location = new Point(point1, point2);
+                        point1 += 193;
+
+                    }
+                    else
+                    {
+                        point1 = 0;
+                        point2 += 211;
+                        uCStol.Location = new Point(point1, point2);
+                    }
+                    
+                    uCStol.ImeStola = stol.Oznaka_Stola;
+                    
+                    this.Controls.Add(uCStol);
+                    
+                }
+            }
+            
+        }
+
+        private void IzbrisiStolove()
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control.GetType() == typeof(UCStol))
+                {
+                    this.Controls.Remove(control);
+                }
+
+            }
+        }
+
+        private void MainKafic_Load(object sender, EventArgs e)
+        {
+            PopuniStolove();
+        }
+
+        private void PromijeniImeStolova()
+        {
+            int brojStolova = 1;
+            for (int i = listaStolova.Count-1; i >=0 ; i--)
+            {
+                listaStolova[i].ImeStola = "Stol" + brojStolova;
+                brojStolova++;
+            }
+        }
+
+        private void PopuniStolove()
+        {
+            listaStolova.Clear();
+            foreach(Control control in this.Controls)
+            {
+                if (control.GetType() == typeof(UCStol))
+                {
+                    listaStolova.Add(control as UCStol);
+                }
+
+            }
+        }
+
+        private void upravaljnjeStolovimaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Upravljanje_Stolovima upravitelj = new Upravljanje_Stolovima(kafic);
+            upravitelj.ShowDialog();
+            RefreshStolovi();
+        }
+
+        private void MainKafic_Activated(object sender, EventArgs e)
+        {
+            RefreshStolovi();
+        }
+
+        private void MainKafic_ResizeEnd(object sender, EventArgs e)
+        {
+            RefreshStolovi();
+        }
+
+        private void upravljanjeNamirnicamaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmNamirniceMain frmNamirnice = new frmNamirniceMain(kafic);
+            frmNamirnice.ShowDialog();
+        }
+
+       
+
+        private void upravljanjeNormativimaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmNormativMain frmNormativ = new frmNormativMain(kafic);
+            frmNormativ.ShowDialog();
+        }
+    }
+}
