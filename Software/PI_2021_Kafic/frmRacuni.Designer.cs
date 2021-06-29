30474d4cc92fcac5d8185fa14f372ecba0364071
Stella Lončarić <stellaloncaric@gmail.com>
2021-06-30 00:11:00 +0200

    Dodana forma za pregled prošlih računa i predviđanje potrošnje po namirnicama.

---
+++ Software/PI_2021_Kafic/frmRacuni.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace PI_2021_Kafic
+{
+    partial class frmRacuni
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvRacuni = new System.Windows.Forms.DataGridView();
+            this.dgvDetalji = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpOd = new System.Windows.Forms.DateTimePicker();
+            this.dtpDo = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbNamirnice = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtKolicina = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAvg = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtOstalo = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRacuni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalji)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvRacuni
+            // 
+            this.dgvRacuni.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRacuni.Location = new System.Drawing.Point(13, 93);
+            this.dgvRacuni.Name = "dgvRacuni";
+            this.dgvRacuni.ReadOnly = true;
+            this.dgvRacuni.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvRacuni.Size = new System.Drawing.Size(653, 233);
+            this.dgvRacuni.TabIndex = 0;
+            this.dgvRacuni.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvRacuni_ColumnHeaderMouseClick);
+            this.dgvRacuni.SelectionChanged += new System.EventHandler(this.dgvRacuni_SelectionChanged);
+            // 
+            // dgvDetalji
+            // 
+            this.dgvDetalji.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetalji.Location = new System.Drawing.Point(13, 345);
+            this.dgvDetalji.Name = "dgvDetalji";
+            this.dgvDetalji.ReadOnly = true;
+            this.dgvDetalji.Size = new System.Drawing.Size(653, 233);
+            this.dgvDetalji.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Racuni:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 329);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Stavke Racuna";
+            // 
+            // dtpOd
+            // 
+            this.dtpOd.Location = new System.Drawing.Point(15, 39);
+            this.dtpOd.Name = "dtpOd";
+            this.dtpOd.Size = new System.Drawing.Size(200, 20);
+            this.dtpOd.TabIndex = 2;
+            this.dtpOd.Value = new System.DateTime(2021, 6, 29, 12, 45, 57, 0);
+            this.dtpOd.ValueChanged += new System.EventHandler(this.dtpOd_ValueChanged);
+            // 
+            // dtpDo
+            // 
+            this.dtpDo.Location = new System.Drawing.Point(289, 39);
+            this.dtpDo.Name = "dtpDo";
+            this.dtpDo.Size = new System.Drawing.Size(200, 20);
+            this.dtpDo.TabIndex = 3;
+            this.dtpDo.ValueChanged += new System.EventHandler(this.dtpDo_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Od:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(289, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Do:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtOstalo);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtAvg);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtKolicina);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.cmbNamirnice);
+            this.groupBox1.Location = new System.Drawing.Point(686, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(194, 198);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Predviđanje potrošnje";
+            // 
+            // cmbNamirnice
+            // 
+            this.cmbNamirnice.FormattingEnabled = true;
+            this.cmbNamirnice.Location = new System.Drawing.Point(9, 49);
+            this.cmbNamirnice.Name = "cmbNamirnice";
+            this.cmbNamirnice.Size = new System.Drawing.Size(121, 21);
+            this.cmbNamirnice.TabIndex = 0;
+            this.cmbNamirnice.SelectedIndexChanged += new System.EventHandler(this.cmbArtikli_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Odaberite Namirnicu:";
+            // 
+            // txtKolicina
+            // 
+            this.txtKolicina.Location = new System.Drawing.Point(9, 89);
+            this.txtKolicina.Name = "txtKolicina";
+            this.txtKolicina.ReadOnly = true;
+            this.txtKolicina.Size = new System.Drawing.Size(100, 20);
+            this.txtKolicina.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Količina na skladištu:";
+            // 
+            // txtAvg
+            // 
+            this.txtAvg.Location = new System.Drawing.Point(9, 128);
+            this.txtAvg.Name = "txtAvg";
+            this.txtAvg.ReadOnly = true;
+            this.txtAvg.Size = new System.Drawing.Size(100, 20);
+            this.txtAvg.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 112);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(176, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Prosjek potrošnje u danom periodu: ";
+            // 
+            // txtOstalo
+            // 
+            this.txtOstalo.Location = new System.Drawing.Point(9, 167);
+            this.txtOstalo.Name = "txtOstalo";
+            this.txtOstalo.ReadOnly = true;
+            this.txtOstalo.Size = new System.Drawing.Size(100, 20);
+            this.txtOstalo.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 151);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Preostalo Dana:";
+            // 
+            // frmRacuni
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 694);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpDo);
+            this.Controls.Add(this.dtpOd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvDetalji);
+            this.Controls.Add(this.dgvRacuni);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmRacuni";
+            this.Text = "Pregled Racuna";
+            this.Load += new System.EventHandler(this.frmRacuni_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRacuni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalji)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvRacuni;
+        private System.Windows.Forms.DataGridView dgvDetalji;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpOd;
+        private System.Windows.Forms.DateTimePicker dtpDo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtAvg;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtKolicina;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbNamirnice;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtOstalo;
+    }
+}
